50decb3241ba6fcfc157fc511f9f0f4d2d2c25cd
Eric Kim <bjkim3671@gmail.com>
2020-04-02 02:41:13 +0900

    fix typo && real1,real2 애플리케이션 properties 추가

--- src/test/java/com/demo/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/demo/springboot/web/ProfileControllerTest.java
@@ -21,7 +21,7 @@ public class ProfileControllerTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    public ovid profile은_인증없이_호출된다() {
+    public void profile은_인증없이_호출된다() {
         String expected = "default";
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
